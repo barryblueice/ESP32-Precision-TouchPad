d06d386dc19d489a6d99595741cb2e28eb7e87a8
barryblueice <barryblueice@outlook.com>
2026-02-10 19:43:17 +0800

    更新中值滤波集成算法

--- main/main/i2c/goodix/goodix_i2c.c
+++ main/main/i2c/goodix/goodix_i2c.c
@@ -111,6 +111,12 @@ typedef enum {
     TOUCH_DRAG
 } touch_state_t;
 
+static uint16_t get_medium(uint16_t n1, uint16_t n2, uint16_t n3) {
+    if ((n1 > n2) ^ (n1 > n3)) return n1;
+    else if ((n2 > n1) ^ (n2 > n3)) return n2;
+    else return n3;
+}
+
 void goodix_i2c_task(void *arg) {
     static uint16_t last_raw_x[5] = {0};
     static uint16_t last_raw_y[5] = {0};
@@ -121,11 +127,11 @@ void goodix_i2c_task(void *arg) {
     static touch_state_t touch_state[5] = {0};
     static uint32_t filtered_x[5] = {0};
     static uint32_t filtered_y[5] = {0};
-
-    // 多点历史，用于严格异常判断
+    
     #define HISTORY_LEN 3
     static uint16_t raw_x_history[5][HISTORY_LEN] = {0};
     static uint16_t raw_y_history[5][HISTORY_LEN] = {0};
+    static int consecutive_errors[5] = {0}; 
 
     uint8_t data[64];
     const uint16_t JUMP_THRESHOLD = 800;
@@ -142,10 +148,8 @@ void goodix_i2c_task(void *arg) {
             for (int i = 0; i < 5; i++) {
                 last_raw_x[i] = 0; last_raw_y[i] = 0;
                 origin_x[i] = 0; origin_y[i] = 0;
-                for (int h = 0; h < HISTORY_LEN; h++) {
-                    raw_x_history[i][h] = 0;
-                    raw_y_history[i][h] = 0;
-                }
+                consecutive_errors[i] = 0;
+                memset(raw_x_history[i], 0, sizeof(raw_x_history[i]));
             }
         }
 
@@ -160,21 +164,33 @@ void goodix_i2c_task(void *arg) {
 
                     for (int id = 0; id < 5; id++) {
                         uint8_t *f_ptr = &data[3 + (id * 5)];
-                        uint8_t status = f_ptr[0];
-                        bool is_valid_touch = (status & 0x01);
-
+                        bool is_valid_touch = (f_ptr[0] & 0x01);
                         uint16_t rx = f_ptr[1] | (f_ptr[2] << 8);
                         uint16_t ry = f_ptr[3] | (f_ptr[4] << 8);
 
-                        if (rx == 0 || ry == 0) {
+                        if (!is_valid_touch || rx == 0 || ry == 0) {
+                            last_raw_x[id] = 0; last_raw_y[id] = 0;
+                            consecutive_errors[id] = 0;
+                            memset(raw_x_history[id], 0, sizeof(raw_x_history[id]));
                             tp_current_state.fingers[id].tip_switch = 0;
                             continue;
                         }
 
+                        for (int h = 0; h < HISTORY_LEN - 1; h++) {
+                            raw_x_history[id][h] = raw_x_history[id][h+1];
+                            raw_y_history[id][h] = raw_y_history[id][h+1];
+                        }
+                        raw_x_history[id][HISTORY_LEN-1] = rx;
+                        raw_y_history[id][HISTORY_LEN-1] = ry;
+
+                        if (raw_x_history[id][0] == 0) continue;
+
+                        uint16_t mx = get_medium(raw_x_history[id][0], raw_x_history[id][1], raw_x_history[id][2]);
+                        uint16_t my = get_medium(raw_y_history[id][0], raw_y_history[id][1], raw_y_history[id][2]);
+
                         int16_t predict_x = raw_x_history[id][HISTORY_LEN-1];
                         int16_t predict_y = raw_y_history[id][HISTORY_LEN-1];
                         int dx_sum = 0, dy_sum = 0, count_valid = 0;
-
                         for (int h = 1; h < HISTORY_LEN; h++) {
                             if (raw_x_history[id][h-1] && raw_x_history[id][h]) {
                                 dx_sum += raw_x_history[id][h] - raw_x_history[id][h-1];
@@ -187,49 +203,42 @@ void goodix_i2c_task(void *arg) {
                             predict_x += dx_sum / count_valid;
                             predict_y += dy_sum / count_valid;
 
-                            const int MAX_JUMP2_STRICT = 300*300;
-                            int dx_jump = rx - predict_x;
-                            int dy_jump = ry - predict_y;
+                            const int MAX_JUMP2_STRICT = 300 * 300;
+                            int dx_jump = mx - predict_x;
+                            int dy_jump = my - predict_y;
 
                             if (dx_jump*dx_jump + dy_jump*dy_jump > MAX_JUMP2_STRICT) {
-                                rx = raw_x_history[id][HISTORY_LEN-1];
-                                ry = raw_y_history[id][HISTORY_LEN-1];
+                                if (consecutive_errors[id] < 2) { 
+                                    mx = last_raw_x[id] ? last_raw_x[id] : mx;
+                                    my = last_raw_y[id] ? last_raw_y[id] : my;
+                                    consecutive_errors[id]++;
+                                } else {
+                                    consecutive_errors[id] = 0;
+                                }
+                            } else {
+                                consecutive_errors[id] = 0;
                             }
                         }
 
-                        for (int h = 0; h < HISTORY_LEN-1; h++) {
-                            raw_x_history[id][h] = raw_x_history[id][h+1];
-                            raw_y_history[id][h] = raw_y_history[id][h+1];
-                        }
-                        raw_x_history[id][HISTORY_LEN-1] = rx;
-                        raw_y_history[id][HISTORY_LEN-1] = ry;
-
                         if (last_raw_x[id] == 0) {
-                            filtered_x[id] = rx << 8;
-                            filtered_y[id] = ry << 8;
-                            origin_x[id] = rx;
-                            origin_y[id] = ry;
+                            filtered_x[id] = mx << 8; filtered_y[id] = my << 8;
+                            origin_x[id] = mx; origin_y[id] = my;
                         } else {
-                            int vx = rx - last_raw_x[id];
-                            int vy = ry - last_raw_y[id];
-                            int alpha_speed = abs(vx) + abs(vy);
-
-                            uint32_t dynamic_alpha;
-                            if (alpha_speed < 3) dynamic_alpha = 64;
-                            else if (alpha_speed < 12) dynamic_alpha = 115;
-                            else dynamic_alpha = 218;
-
-                            filtered_x[id] = (dynamic_alpha * (rx << 8) + (256 - dynamic_alpha) * filtered_x[id]) >> 8;
-                            filtered_y[id] = (dynamic_alpha * (ry << 8) + (256 - dynamic_alpha) * filtered_y[id]) >> 8;
+                            int vx = mx - last_raw_x[id];
+                            int vy = my - last_raw_y[id];
+                            int speed = abs(vx) + abs(vy);
+                            uint32_t alpha = (speed < 3) ? 64 : (speed < 12 ? 115 : 218);
+                            filtered_x[id] = (alpha * (mx << 8) + (256 - alpha) * filtered_x[id]) >> 8;
+                            filtered_y[id] = (alpha * (my << 8) + (256 - alpha) * filtered_y[id]) >> 8;
                         }
 
                         uint16_t fx = (uint16_t)(filtered_x[id] >> 8);
                         uint16_t fy = (uint16_t)(filtered_y[id] >> 8);
 
-                        int dx = abs((int)rx - (int)origin_x[id]);
-                        int dy = abs((int)ry - (int)origin_y[id]);
+                        int dx_org = abs((int)mx - (int)origin_x[id]);
+                        int dy_org = abs((int)my - (int)origin_y[id]);
 
-                        if (touch_state[id] == TOUCH_TAP_CANDIDATE && (dx > TAP_DEADZONE || dy > TAP_DEADZONE)) {
+                        if (touch_state[id] == TOUCH_TAP_CANDIDATE && (dx_org > TAP_DEADZONE || dy_org > TAP_DEADZONE)) {
                             touch_state[id] = TOUCH_DRAG;
                             tap_frozen[id] = false;
                         }
@@ -248,19 +257,18 @@ void goodix_i2c_task(void *arg) {
                             tp_current_state.fingers[id].y = fy;
                         }
 
-                        if (!tap_frozen[id] && last_raw_x[id] != 0 && abs((int)rx - (int)last_raw_x[id]) > JUMP_THRESHOLD) {
+                        if (!tap_frozen[id] && last_raw_x[id] != 0 && abs((int)mx - (int)last_raw_x[id]) > JUMP_THRESHOLD) {
                             tp_current_state.fingers[id].x = last_raw_x[id];
                             tp_current_state.fingers[id].y = last_raw_y[id];
+                        } else {
+                            last_raw_x[id] = mx;
+                            last_raw_y[id] = my;
                         }
 
-                        last_raw_x[id] = rx;
-                        last_raw_y[id] = ry;
-
-                        tp_current_state.fingers[id].tip_switch = is_valid_touch ? 1 : 0;
+                        tp_current_state.fingers[id].tip_switch = 1;
                         tp_current_state.fingers[id].contact_id = id;
                         has_data = true;
                     }
-
                 } else if (data[2] == 0x01) {
                     current_mode = MOUSE_MODE;
                     mouse_current_state.x = (int8_t)data[4];
