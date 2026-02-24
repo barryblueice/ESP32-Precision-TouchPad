aa562105722f5e79c4e5c7cf42b80d82c19d0ae7
barryblueice <barryblueice@outlook.com>
2026-02-24 20:49:58 +0800

    进一步更新和完善程序，添加gitignore

--- dfu/main.py
+++ dfu/main.py
@@ -1,37 +1,56 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QStatusBar
+from usb_module import USBCommunicatorThread
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("ESP32 PTP DFU")
-
+        
         self.setStatusBar(QStatusBar(self))
-        self.statusBar().showMessage("No ESP32 PTP device found")
+        self.statusBar().showMessage("Initializing...")
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        
         layout = QVBoxLayout(central_widget)
-        layout.setContentsMargins(10, 10, 10, 10) 
+        layout.setContentsMargins(10, 10, 10, 10)
 
-        self.button = QPushButton("Enter DFU")
-        
-        self.button.setFixedSize(300, 50)
-        
-        self.button.clicked.connect(self.on_button_clicked)
-        layout.addWidget(self.button)
+        self.btn_action = QPushButton("Waiting for connect...")
+        self.btn_action.setFixedSize(300, 50)
+        self.btn_action.setEnabled(False)
+        self.btn_action.clicked.connect(self.on_button_clicked)
+        layout.addWidget(self.btn_action)
 
-        self.adjustSize()
+        self.usb_thread = USBCommunicatorThread()
         
+        self.usb_thread.device_event.connect(self.handle_device_event)
+        
+        self.usb_thread.status_message.connect(self.statusBar().showMessage)
+        
+        self.usb_thread.start()
+
+        self.adjustSize() 
         self.setFixedSize(self.sizeHint())
 
+    def handle_device_event(self, is_connected):
+        if is_connected:
+            self.btn_action.setEnabled(True)
+            self.btn_action.setText("Enter DFU Mode")
+        else:
+            self.btn_action.setEnabled(False)
+            self.btn_action.setText("Waiting for connect...")
+
     def on_button_clicked(self):
-        self.statusBar().showMessage("Entering DFU mode...", 2000)
+        self.statusBar().showMessage("Sending DFU command...", 2000)
+
+    def closeEvent(self, event):
+        self.usb_thread.stop()
+        event.accept()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
---
+++ dfu/usb_module.py
@@ -0,0 +1,62 @@
+import usb.core
+import usb.backend.libusb1
+from PySide6.QtCore import QThread, Signal
+import hid
+import os
+
+VID = 0x0D00
+PID = 0x072A
+REPORT_SIZE = 64
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+lib_path = os.path.join(current_dir, 'libusb-1.0.dll')
+backend = usb.backend.libusb1.get_backend(find_library=lambda x: lib_path)
+
+class USBCommunicatorThread(QThread):
+    device_event = Signal(bool)
+    status_message = Signal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.running = True
+        self.device_present = False
+        self.dev = hid.device()
+
+    def run(self):
+        self.status_message.emit("USB Monitor started...")
+        while self.running:
+            try:
+                dev_found = usb.core.find(idVendor=VID, idProduct=PID, backend=backend)
+                
+                is_hid = False
+                if dev_found:
+                    for cfg in dev_found:
+                        for intf in cfg:
+                            if intf.bInterfaceClass == 0x03:
+                                is_hid = True
+                                break
+                
+                if is_hid and not self.device_present:
+                    try:
+                        self.dev.open(VID, PID)
+                        self.device_present = True
+                        self.device_event.emit(True)
+                        self.status_message.emit(f"Device connected: (VID:{hex(VID)} PID:{hex(PID)})")
+                    except Exception as e:
+                        self.status_message.emit(f"Open Failed: {str(e)}")
+
+                elif not is_hid and self.device_present:
+                    self.device_present = False
+                    self.dev.close()
+                    self.device_event.emit(False)
+                    self.status_message.emit("Device disconnected")
+                    
+            except Exception as e:
+                pass
+
+            self.msleep(500)
+
+    def stop(self):
+        self.running = False
+        self.dev.close()
+        self.wait()
